3804619d6c285d8d975ae83b8d22d93df7e940a1
Nam Pham <nampdn@protonmail.com>
2018-05-05 09:40:00 +0700

    refactor(importer): add AIRTABLE_BASE_ID environment

--- server/importer/src/config/index.ts
+++ server/importer/src/config/index.ts
@@ -1,2 +1,3 @@
 export const PORT = process.env.IMPORT_PORT || 7002
 export const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY || console.error('Please specify Airtable API key!')
+export const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID || console.error('Please specify Airtable Base Id!')
