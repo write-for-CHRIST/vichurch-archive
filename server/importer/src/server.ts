a1a0a6bedaaf3d06d8a25018c00054ab0f9e71a4
luuthienkhanh <luuthienkhanh@gmail.com>
2018-05-16 22:53:46 +0700

    refactor(importer): replace getTableData to getTableChurch and getTableFloor

--- server/importer/src/server.ts
+++ server/importer/src/server.ts
@@ -3,22 +3,15 @@ import * as Koa from 'koa'
 import {routes} from './routes'
 import {PORT} from './config'
 import {getTableData} from './resources/airtable/airtable'
+import {getTableChurch} from './resources/airtable/church.airtable'
+import {getTableFloor} from './resources/airtable/floor.airtable'
 
 const app = new Koa()
 app.use(routes)
 app.listen(PORT)
 
 const tableChurch = {name: 'Church', fields: ['domain', 'name', '_floors']}
-getTableData(tableChurch)
-  .then(result => {
-    console.log(JSON.stringify(result, null, 2))
-  })
-  .catch(err => console.error(err))
+getTableChurch(tableChurch)
 
 const tableFloor = {name: 'Floor', fields: ['domain', '_church', '_rooms']}
-getTableData(tableFloor)
-  .then(result => {
-    console.log(JSON.stringify(result, null, 2))
-  })
-  .catch(err => console.error(err))
-console.log(`Importer is running on port ${PORT}`)
+getTableFloor(tableFloor)
